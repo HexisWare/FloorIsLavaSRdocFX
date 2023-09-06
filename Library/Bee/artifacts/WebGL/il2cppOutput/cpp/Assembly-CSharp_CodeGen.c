802da3dfb9e4dc1ef32f3992cf8f8e8eccc399e2
Patrik Sokolowski <wunderward@gmail.com>
2023-09-06 08:56:33 -0400

    bad practice, but everything

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,61 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ScrollingUVs_Layers::LateUpdate()
+extern void ScrollingUVs_Layers_LateUpdate_m05A3B77B93DF5709791418FD61541D9E1BC0416D (void);
+// 0x00000002 System.Void ScrollingUVs_Layers::.ctor()
+extern void ScrollingUVs_Layers__ctor_m940CE88EA0B7EC37C31FE02CC263BA786EA6499D (void);
+// 0x00000003 System.Void Grow::Start()
+extern void Grow_Start_m5A0AE07358DD428566656F0E800F164283995822 (void);
+// 0x00000004 System.Void Grow::Update()
+extern void Grow_Update_m8C65F713122568049D9C7022135BFD2E98460891 (void);
+// 0x00000005 System.Void Grow::.ctor()
+extern void Grow__ctor_m62E1B75EEF6A2A340C69ED9377BD9A0E0130014E (void);
+static Il2CppMethodPointer s_methodPointers[5] = 
+{
+	ScrollingUVs_Layers_LateUpdate_m05A3B77B93DF5709791418FD61541D9E1BC0416D,
+	ScrollingUVs_Layers__ctor_m940CE88EA0B7EC37C31FE02CC263BA786EA6499D,
+	Grow_Start_m5A0AE07358DD428566656F0E800F164283995822,
+	Grow_Update_m8C65F713122568049D9C7022135BFD2E98460891,
+	Grow__ctor_m62E1B75EEF6A2A340C69ED9377BD9A0E0130014E,
+};
+static const int32_t s_InvokerIndices[5] = 
+{
+	3171,
+	3171,
+	3171,
+	3171,
+	3171,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	5,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
